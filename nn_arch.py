281e5fb622d447b1916f723a101bff1592ad3870
Robert Deyk <36889034+Rustyiller@users.noreply.github.com>
2021-02-03 10:14:32 +0100

    lenet

--- nn_arch.py
+++ nn_arch.py
@@ -6,6 +6,7 @@ from tensorflow.keras.layers import Flatten
 from tensorflow.keras.layers import Activation
 from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Dropout
+from tensorflow.keras.layers import AveragePooling2D
 
 
 class SmallerVGGNet:
@@ -54,5 +55,42 @@ class SmallerVGGNet:
         # sigmoid activation for multi-label classification
         model.add(Dense(classes))
         model.add(Activation("sigmoid"))
+
+        return model
+
+
+class LeNet5:
+
+    def build(width, height, depth, classes):
+        # Network architecture:
+        # INPUT => CONV => RELU => POOL => CONV => RELU => POOL => FC => RELU => FC => SOFTMAX
+
+        model = Sequential()
+        inputShape = (height, width, depth)
+
+        # C1 Convolutional Layer. Padding='same' gives the same output as input, so the input images could be treated as 32x32 px
+        model.add(
+            Conv2D(6, kernel_size=(5, 5), strides=(1, 1), activation='tanh', input_shape=inputShape, padding='same'))
+
+        # S2 Pooling Layer
+        model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+        # C3 Convolutional Layer
+        model.add(Conv2D(16, kernel_size=(5, 5), strides=(1, 1), activation='tanh', padding='valid'))
+
+        # S4 Pooling Layer
+        model.add(AveragePooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+
+        # C5 Fully Connected Convolutional Layer
+        model.add(Conv2D(120, kernel_size=(5, 5), strides=(1, 1), activation='tanh', padding='valid'))
+
+        # Flatten the CNN output so that we can connect it with fully connected layers
+        model.add(Flatten())
+
+        # FC6 Fully Connected Layer
+        model.add(Dense(84, activation='tanh'))
+
+        # Output Layer with softmax activation
+        model.add(Dense(classes, activation='softmax'))
 
         return model
